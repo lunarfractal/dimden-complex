0b4f79ac0c03e8f1ce6aa8be441be9c3cd09f1ef
lunarfractal <xxs239239@gmail.com>
2025-07-03 19:52:50 +0200

    init

---
+++ crash.cpp
@@ -0,0 +1,171 @@
+#define ASIO_STANDALONE
+
+#include <websocketpp/config/asio_client.hpp>
+#include <websocketpp/client.hpp>
+
+#include <asio.hpp>
+#include <asio/steady_timer.hpp>
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <memory>
+#include <vector>
+
+
+using websocketpp::connection_hdl;
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+typedef websocketpp::client<websocketpp::config::asio_tls_client> ws_client;
+
+class WebSocket {
+public:
+    WebSocket(const std::string& uri, std::shared_ptr<asio::io_context> io_context)
+        : client_(), uri_(uri), reconnect_timer_(*io_context), message_timer_(*io_context),
+           reconnect_attempts_(0), reconnect_interval_(1000), max_reconnect_attempts_(4),
+           message_interval_(1000) {
+        client_.clear_access_channels(websocketpp::log::alevel::all);
+        client_.init_asio(io_context.get());
+
+        client_.set_open_handler([this](connection_hdl hdl) {
+            hdl_ = hdl;
+            client_.get_alog().write(websocketpp::log::alevel::connect, "Connected!");
+            reconnect_attempts_ = 0;
+
+            start_message_timer();
+        });
+
+        //client_.set_message_handler([this](connection_hdl, message_ptr msg) {});
+
+        client_.set_fail_handler([this](connection_hdl hdl) {
+            auto con = client_.get_con_from_hdl(hdl);
+            client_.get_elog().write(websocketpp::log::elevel::rerror, "Error: " + con->get_ec().message());
+
+            start_reconnect_timer();
+        });
+
+        client_.set_tls_init_handler([](websocketpp::connection_hdl) {
+            return websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::tlsv12_client);
+        });
+
+
+        client_.set_close_handler([this](connection_hdl) {
+            start_reconnect_timer();
+        });
+
+        client_.get_alog().write(websocketpp::log::alevel::app, "Connecting to " + uri + "...");
+
+        connect();
+    }
+
+    void connect() {
+        if(reconnect_attempts_ > max_reconnect_attempts_) {
+            return;
+        }
+
+        reconnect_attempts_++;
+      
+        websocketpp::lib::error_code ec;
+        auto con = client_.get_connection(uri_, ec);
+        if (ec) {
+            client_.get_elog().write(websocketpp::log::elevel::rerror, "socket error: " + ec.message());
+            return;
+        }
+
+        con->set_proxy("http://localhost:8087");
+        client_.connect(con);
+    }
+
+    void set_reconnect_interval(int n) {
+        reconnect_interval_ = n;
+    }
+
+    void set_reconnect_attempts(int n) {
+        max_reconnect_attempts_ = n;
+    }
+
+    void set_message_interval(int n) {
+        message_interval_ = n;
+    }
+
+    void start_reconnect_timer() {
+        reconnect_timer_.expires_after(std::chrono::milliseconds(reconnect_interval_));
+        reconnect_timer_.async_wait([this](const websocketpp::lib::error_code& ec) {
+            if (ec) {
+                client_.get_elog().write(websocketpp::log::elevel::rerror, "Reconnect timer failed: " + ec.message());
+            } else {
+                connect();
+            }
+        });
+    }
+
+    void start_message_timer() {
+        message_timer_.expires_after(std::chrono::milliseconds(message_interval_));  // reset before wait
+
+        message_timer_.async_wait([this](const websocketpp::lib::error_code& ec) {
+            if (ec) {
+                client_.get_elog().write(websocketpp::log::elevel::rerror, "Message timer failed: " + ec.message());
+               return;
+            }
+
+            static int16_t int16arr[3] = {1223, 1441, 3861};
+
+            websocketpp::lib::error_code send_ec;
+            client_.send(hdl_, int16arr, sizeof(int16arr), websocketpp::frame::opcode::binary, send_ec);
+
+            if (send_ec) {
+                client_.get_elog().write(websocketpp::log::elevel::rerror,
+                    "Send failed: " + send_ec.message());
+            }
+
+            start_message_timer();
+        });
+    }
+
+
+private:
+    ws_client client_;
+    std::string uri_;
+
+    connection_hdl hdl_;
+
+    asio::steady_timer reconnect_timer_;
+    asio::steady_timer message_timer_;
+
+    int reconnect_attempts_;
+    int reconnect_interval_;
+    int max_reconnect_attempts_;
+    int message_interval_;
+};
+
+int main() {
+    const int thread_count = 4;
+    std::shared_ptr<asio::io_context> io_context = std::make_shared<asio::io_context>();
+    auto work = asio::make_work_guard(*io_context);
+
+    std::vector<std::thread> thread_pool;
+    for (int i = 0; i < thread_count; ++i) {
+        thread_pool.emplace_back([io_context]() {
+            io_context->run();
+        });
+    }
+
+    std::vector<std::unique_ptr<WebSocket>> clients;
+
+    std::string uri = "wss://echo2.glitch.me";
+
+    for(int i = 0; i < 4; i++) {
+        auto client = std::make_unique<WebSocket>(uri, io_context);
+        clients.push_back(std::move(client));
+    }
+  
+    std::cout << "Type something to stop" << std::endl;
+    std::cin.get();
+  
+    io_context->stop();
+    for (auto& t : thread_pool) {
+        t.join();
+    }
+
+    return 0;
+}
